0d8d3ec96a2c1ef6301165abeb1b14d2a7aa6713
Gonzalo Othacehe <gonzalo.othacehe@valtech.com>
2021-12-26 13:29:53 -0300

    advanced

--- js/main.js
+++ js/main.js
@@ -1,44 +1,63 @@
 import { pedirUsuarios } from "./http-provider.js";
-import { dibujarPagina, filtrarBusqueda, ordenarAlpha, vaciarContenedor } from "./userInterface.js";
+import { añadirBotonesPaginas, dibujarPagina, filtrarBusqueda, ordenarAlpha } from "./userInterface.js";
 let usuarios;
-
+let usuariosCopy; // Mantengo una copia del arreglo original
 
 // Listar usuarios (API)
 const init = async () => {
     console.time("fetch time test");
     usuarios = await pedirUsuarios();
     console.timeEnd("fetch time test");
+    usuariosCopy = [...usuarios]; // Copio
 
     ordenarAlpha(usuarios, "nameasc");
-    // Paginar usuarios (cada 10)
-    dibujarPagina(usuarios, 1);
-};
 
+    dibujarPagina(usuarios, 1);
+    añadirBotonesPaginas(usuarios); //Añade la cantidad necesaria de botones.
+    eventoBotonesPaginas();
+    eventos();
+};
 
 const eventos = () => {
     // Buscar usuarios -> search field
     const search__input = document.querySelector("#search__input");
     search__input.addEventListener("input", e => {
-        let arrayBusqueda = filtrarBusqueda(usuarios, e.target.value);
-        vaciarContenedor();
-        dibujarPagina(arrayBusqueda, 1);
+        usuarios = filtrarBusqueda(usuariosCopy, e.target.value); // muto usuarios.
+        dibujarPagina(usuarios, 1);
+        añadirBotonesPaginas(usuarios);
+        eventoBotonesPaginas();
     });
 
     // Ordenar usuarios (Alfabeticamente por nombre ASC y DESC)
     const sort__input = document.querySelector("#sort__input");
     sort__input.addEventListener("change", e => {
         ordenarAlpha(usuarios, e.target.value);
-        vaciarContenedor();
         dibujarPagina(usuarios, 1);
     });
 
     // paginacion previo
-    // paginacion siguiente
-    // paginacion numeral
-}
+    // const pagination__btnprev = document.querySelector(".pagination__btnprev");
+    // pagination__btnprev.addEventListener("click", () => {
+    //     dibujarPagina(usuarios, N);
+    // });
 
+    // paginacion siguiente
+    // const pagination__btnnext = document.querySelector(".pagination__btnnext");
+    // pagination__btnnext.addEventListener("click", () => {
+    //     dibujarPagina(usuarios, N);
+    // });
+};
+
+const eventoBotonesPaginas = () => {
+    const pagination__buttons = document.querySelectorAll(".pagination__btn");
+    for (let button of pagination__buttons) {
+        button.addEventListener("click", e => {
+            dibujarPagina(usuarios, e.target.innerText);
+
+            pagination__buttons.forEach(button => button.classList.remove("current__page"));
+            e.target.classList.add("current__page");
+        });
+    }
+};
 
 init();
-eventos();
-
-
--- js/userInterface.js
+++ js/userInterface.js
@@ -1,5 +1,7 @@
 const users__container = document.querySelector(".users__container");
 
+const pagination__btncontainer = document.querySelector(".pagination__btncontainer");
+
 const PAGE_SIZE = 10;
 
 const showing__max = document.querySelector(".showing__max"); // 28 Candidates
@@ -52,33 +54,55 @@ const dibujarUsuario = usuario => {
     users__container.append(card);
 };
 
+// Devuelve un arreglo conteniendo los usuarios de la página número N.
 const paginateArray = (array, page_number) => {
     return array.slice((page_number - 1) * PAGE_SIZE, page_number * PAGE_SIZE);
-    // arreglo de 100, size de 10, number de 10.
-    // pagina 2: slice(2-1 * 10, 2 * 10) = slice(10, 20)
+
+    // Arreglo de 95, size de 10, number 9
+    // pagina 9: slice(9-1 * 10, 9 * 10) = slice(80, 90) -> [80,89]
+    // pagina 10: slice(10-1 * 10, 10 * 10) = slice(90, 100) -> [90,99] -> [90, 94]
+    // del método slice: si el parametro final es mayor al length, extrae hasta el fin del length.
 };
 
-const contabilizarUsuarios = (usuarios) => {
-    let cantidadMostrada = users__container.children.length;
-    let cantidadTotal = usuarios.length;
-    showing__max.innerHTML = `
-        ${cantidadTotal} Candidates
-    `; // 28 Candidates
-    showing__number.innerHTML = `
-        ${cantidadMostrada} - ${PAGE_SIZE}
-    `; // 1 - 10;
-};
+// usuariosTotal puede o no ser el arreglo ya filtrado por busqueda.
+// const contabilizarUsuarios = (usuariosTotal, usuariosPagina, page_number) => {
+//     let cantidadTotal = usuariosTotal.length;
+//     let cantidadMostrada = users__container.children.length;
 
-const dibujarPagina = (array, page_number) => {
-    paginateArray(array, page_number).forEach(usuario => dibujarUsuario(usuario));
-    contabilizarUsuarios(array);
-};
+//     let firstUserShown
+//     let lastUserShown;
 
-const vaciarContenedor = () => {
-    while (users__container.children.length > 0) {
-        users__container.children[0].remove();
-        console.log("vaciarContenedor() - contenedor vaciado.");
+//     showing__max.innerHTML = `
+//         ${cantidadTotal} Candidates
+//     `; // 28 Candidates
+//     showing__number.innerHTML = `
+//         ${firstUserShown} - ${lastUserShown}
+//     `; // 1 - 10;
+// };
+
+const vaciarContenedor = contenedor => {
+    while (contenedor.children.length > 0) {
+        contenedor.children[0].remove();
     }
+};
+
+const añadirBotonesPaginas = usuarios => {
+    vaciarContenedor(pagination__btncontainer);
+
+    let cantidadPaginas = Math.ceil(usuarios.length / PAGE_SIZE);
+    for (let i = 1; i <= cantidadPaginas; i++) {
+        const botonPagina = document.createElement("button");
+        botonPagina.classList.add("pagination__btn");
+        botonPagina.innerText = i;
+        pagination__btncontainer.append(botonPagina);
+    }
+};
+
+const dibujarPagina = (usuariosTotal, page_number) => {
+    vaciarContenedor(users__container);
+    let usuariosPagina = paginateArray(usuariosTotal, page_number);
+    usuariosPagina.forEach(usuario => dibujarUsuario(usuario));
+    // contabilizarUsuarios(usuariosTotal, usuariosPagina, page_number);
 };
 
 const filtrarBusqueda = (usuarios, valor) => {
@@ -109,5 +133,4 @@ const ordenarAlpha = (array, parametro) => {
     }
 };
 
-
-export { dibujarPagina, vaciarContenedor, filtrarBusqueda, ordenarAlpha };
+export { dibujarPagina, filtrarBusqueda, ordenarAlpha, añadirBotonesPaginas };
